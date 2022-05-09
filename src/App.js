b48dbe47bc82ead0ebb283b7cb9793af1bf4f8fd
Hernani Fernandes <hernanif1@gmail.com>
2022-05-09 16:25:29 +0200

    init repo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { IntlProvider, useIntl, defineMessages } from "react-intl";
+import messagesEn from "./lang/en.json";
 
-function App() {
+const messages = defineMessages({
+  explanation: {
+    id: "cookieConsentBanner.explanation",
+    defaultMessage:
+      "{brandNameTitle} uses cookies to make the website, the chat function and the ordering platform work optimally, to analyze the use of the website and to enable advertisements to fit your personal preferences. [Please read our cookie policy to find out more](/cookieverklaring).",
+  },
+});
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <IntlProvider messages={messagesEn} locale="en-US" defaultLocale="en">
+      <Text />
+      asf
+    </IntlProvider>
   );
 }
 
-export default App;
+const Text = () => {
+  const intl = useIntl();
+  const tex = intl.formatMessage(messages.explanation, {
+    brandNameTitle: "Duxxie.nl",
+  });
+
+  return <p>{tex}</p>;
+};
--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import "react-app-polyfill/ie11";
+import "react-app-polyfill/stable";
+import "fast-text-encoding/text";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
